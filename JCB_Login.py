8ae2ab58a4bce902e6d13e74de1b67179a2efb6d
vincent Huang <36068948+vincent20180202@users.noreply.github.com>
2018-02-05 09:14:23 +0800

    Update JCB_Login.py

--- JCB_Login.py
+++ JCB_Login.py
@@ -33,3 +33,5 @@ driver.find_element_by_id('txtEasyCard4').send_keys('7123')
 
 driver.find_element_by_id('recaptcha-anchor').click()
 driver.find_element_by_xpath('/html/body/div[2]/div[3]/div[1]/div/div/span/div[5]').click()
+
+#test
